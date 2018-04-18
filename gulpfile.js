e28c412c0e3fa8f5514f9be4430ae721cd56ffc1
Alessandro Marin <alessandro.m316@gmail.com>
2018-04-18 12:30:40 -0400

    adds some gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,36 @@
-var gulp        = require('gulp');
-var runSequence = require('run-sequence');
-var sass        = require('gulp-sass');
+const gulp          = require('gulp')
+const runSequence   = require('run-sequence')
+const sass          = require('gulp-sass')
+const babel         = require('gulp-babel')
 
 var distFolder = './static/css';
 
 gulp.task('build:sass', function () {
     return gulp.src('./scss/main.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest(distFolder));
+        .pipe(gulp.dest(distFolder))
 });
 
 gulp.task('watch:sass', function () {
-    return gulp.watch('./scss/**/*.scss', ['build:sass']);
+    return gulp.watch('./scss/**/*.scss', ['build:sass'])
 });
+
+gulp.task('build:js', function() {
+    return gulp.src('./js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('bundle.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./static/js'))
+})
 
 /* MAIN GULP TASKS */
 gulp.task('default', function(cb) {
-    runSequence('build:sass', cb);
+    runSequence('build:sass', cb)
 });
+
 gulp.task('watch', function(cb) {
-    runSequence('build:sass', 'watch:sass', cb);
+    runSequence('build:sass', 'watch:sass', cb)
 });
