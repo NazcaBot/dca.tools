1f0dac41f6734a20b74197a3e215ab4f032d6776
Alessandro Marin <alessandro.m316@gmail.com>
2018-04-18 16:59:11 -0400

    selecting a coin downloads chart

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,33 @@
 // var markets = {}
-var coins = []
+var coinNames = []
+var nameToSymbol = {}
 
 // Get available markets
 axios.get("https://min-api.cryptocompare.com/data/all/coinlist")
 .then(function(res) {
     var res = res.data.Data
-
     for (m in res) {
-        coins.push(res[m].FullName)
+        coinNames.push(res[m].FullName)
+        nameToSymbol[res[m].FullName] = res[m]
     }
 })
-
+.catch(function (err) {
+    console.log(err)
+})
 
 var demo = new autoComplete({
     selector: '#base_currency',
     minChars: 1,
     source: function(term, suggest){
         term = term.toLowerCase();
-        var choices = coins
-        var suggestions = [];
-        for (i=0;i<choices.length;i++)
-            if (~choices[i].toLowerCase().indexOf(term)) suggestions.push(choices[i]);
+        var suggestions = coinNames.filter(function (c) {
+            return ~c.toLowerCase().indexOf(term)
+        })
         suggest(suggestions);
     },
     onSelect: function(e, term, item) {
-        console.log(term, item)
+        var base = nameToSymbol[term].Symbol
+        var quote = "USD"
+        fetchChartData(quote, base)
     }
 })
