2a6346a686d24e81c22cd2090fa1fb7a9bc0a302
Alessandro Marin <alessandro.m316@gmail.com>
2018-04-18 16:38:25 -0400

    copy

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,4 +1,4 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 80},
+var margin = {top: 20, right: 20, bottom: 25, left: 80},
             width = 960 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
 
