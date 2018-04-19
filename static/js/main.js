6ca162751683dfbe2b6e88a678c8ee67808b6d5a
Evelyn-McDonald <evel.mcdonald@gmail.com>
2018-04-19 11:07:53 -0400

    more input field logic

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,10 @@
 var coins = []
 var coinNames = []
 var nameToSymbol = {}
+
 var quote = 'USD'
 var base = 'BTC'
+var currentData = {}
 
 // Get available markets
 axios.get("https://min-api.cryptocompare.com/data/all/coinlist")
@@ -48,8 +50,8 @@ var demo = new autoComplete({
 
 
 var fetchCoinStats = function(coin, quote, base) {
-    quote = quote.toUpperCase()
-    base = base.toUpperCase()
+    quote = this.quote.toUpperCase()
+    base = this.base.toUpperCase()
     axios.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms="+base+"&tsyms="+quote)
     .then(function(res) {
         var display = res.data.DISPLAY[base][quote]
@@ -62,7 +64,7 @@ var fetchCoinStats = function(coin, quote, base) {
             marketcap
 
         */
-        var data = {
+        currentData = {
             icon: coin.ImageUrl,
             price: { raw: raw.PRICE, display: display.PRICE },
             price24hrChange: { raw: raw.CHANGEPCT24HOUR, display: display.CHANGEPCT24HOUR },
@@ -70,9 +72,20 @@ var fetchCoinStats = function(coin, quote, base) {
             marketCap: { raw: raw.MKTCAP, display: display.MKTCAP }
         }
 
-        console.log(data)
+        target_rate_INPUT.value = currentData.price.raw
+        if (current_avgrate_CALC && initial_balance_CALC) updateInitialStats()
+
+        console.log(currentData)
     })
 }
+
+var quote_select = document.getElementById("quote_currency");
+quote_select.addEventListener('change', function(e) {
+    quote = e.target.value
+    document.querySelectorAll('.tip.quote').forEach(function(tip) {
+        tip.innerHTML = quote.toUpperCase()
+    })
+})
 
 
 /* Element selectors */
@@ -129,27 +142,67 @@ function debounce(func, wait, immediate) {
     };
 }
 
-var updateInitialRate = debounce(e => {
-    current_avgrate_CALC = e.target.value.trim().replace(/[^\d.-]/g, '')
-    current_avgrate_DISPLAY.innerHTML = `at ${current_avgrate_CALC}`
+function sanitize(input) {
+    return parseFloat(input.trim().replace(/[^\d.-]/g, '')) || 0
+}
+
+// Initials
+var updateInitialRate = debounce(function(e) {
+    current_avgrate_CALC = sanitize(e.target.value)
+    current_avgrate_DISPLAY.innerHTML = `at ${current_avgrate_CALC }`
     initial_rate_INPUT.value = current_avgrate_CALC
 
     if (initial_balance_CALC) updateInitialStats()
 })
 
-var updateInitialBalance = debounce(e => {
-    initial_balance_CALC = e.target.value.trim().replace(/[^\d.-]/g, '')
-    initial_balance_DISPLAY.innerHTML = `Total holdings: ${initial_balance_CALC}`
-    initial_num_coins_INPUT = initial_balance_CALC
+var updateInitialBalance = debounce(function(e) {
+    initial_balance_CALC = sanitize(e.target.value)
+    initial_balance_DISPLAY.innerHTML = `Total holdings: ${initial_balance_CALC} ${base.toUpperCase()}`
+    initial_num_coins_INPUT.value = initial_balance_CALC
 
     if (current_avgrate_CALC) updateInitialStats()
 })
 
-var updateInitialStats = () => {
-    initial_value_CALC = parseFloat((current_avgrate_CALC * initial_balance_CALC).toFixed(8))
-    initial_value_DISPLAY.innerHTML = `Initial value: ${initial_value_CALC} ${quote}`
+function updateInitialStats() {
+    initial_value_CALC = current_avgrate_CALC * initial_balance_CALC
+    initial_value_DISPLAY.innerHTML = `Initial value: ${parseFloat(initial_value_CALC.toFixed(8))} ${quote.toUpperCase()}`
+
+    if (currentData.price) {
+        current_value_CALC = currentData.price.raw * initial_balance_CALC
+        current_value_DISPLAY.innerHTML = `Current value: ${parseFloat(current_value_CALC.toFixed(8))} ${quote.toUpperCase()}`
+
+        current_position_CALC = initial_value_CALC ? (current_value_CALC - initial_value_CALC) / initial_value_CALC * 100 : 0
+        current_position_DISPLAY.innerHTML = `${current_position_CALC.toFixed(2)}%`
+    }
 }
 
 initial_rate_INPUT.addEventListener("keyup", updateInitialRate)
 initial_num_coins_INPUT.addEventListener("keyup", updateInitialBalance)
+
+
+// Targets
+var updateTargetPosition = debounce(function(e) {
+    target_position_CALC = sanitize(e.target.value)
+    target_position_DISPLAY.innerHTML = `${target_position_CALC.toFixed(2)}%`
+    target_position_INPUT.value = target_position_CALC
+})
+
+var updateTargetRate = debounce(function(e) {
+    target_avgrate_CALC = sanitize(e.target.value)
+    target_rate_INPUT.value = target_avgrate_CALC
+})
+
+var updateTargetInvestment = debounce(function(e) {
+    target_value_CALC = sanitize(e.target.value)
+    target_value_DISPLAY.innerHTML = `Target value: ${target_value_CALC + initial_value_CALC} ${quote.toUpperCase()}`
+    target_investment_INPUT.value = target_value_CALC
+})
+
+function updateTargetStats() {
+
+}
+
+target_position_INPUT.addEventListener("keyup", updateTargetPosition)
+target_rate_INPUT.addEventListener("keyup", updateTargetRate)
+target_investment_INPUT.addEventListener("keyup", updateTargetInvestment)
 
